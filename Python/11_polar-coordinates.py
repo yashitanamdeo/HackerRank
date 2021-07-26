299e585fa81ab76a8c5909b2a770e3d2032c931d
Yashita-Namdeo <yashita.namdeo2000@gmail.com>
2021-07-26 22:24:03 +0530

    python - finding the percentage

---
+++ Python/11_polar-coordinates.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+
+complexNumber = complex(input())
+modulus = cmath.phase(complexNumber)
+phaseAngle = abs(complexNumber)
+print(phaseAngle)
+print(modulus)
