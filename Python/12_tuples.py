06af2d4d8ae93d475a979b583091ec909eb9b758
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-08-24 20:46:57 +0530

    python - tuples

--- Python/12_tuples.py
+++ Python/12_tuples.py
@@ -2,4 +2,4 @@ if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
     t = tuple(integer_list)
-    print(hash(t))
+    print(hash(t))   #hash() is one of the functions in the __builtins__ module, so it need not be imported.
