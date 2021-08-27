66f584565a3da9a6fb7cdd9df1b3dd7e0008dd95
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-08-27 23:15:25 +0530

    update text wrap

--- Python/20_text_wrap.py
+++ Python/20_text_wrap.py
@@ -1,5 +1,6 @@
 import textwrap
 
+# without textwrap
 def wrap(string, max_width):
     for i in range(0,len(string),max_width):
         result = string[i:i+max_width]
@@ -7,6 +8,11 @@ def wrap(string, max_width):
             print(result)
         else:
             return result
+
+# with textwrap
+def wrap(string, max_width):
+    wrappedText = " ".join(textwrap.wrap(string,max_width))
+    return(textwrap.fill(wrappedText,max_width))
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
